3d7911ed796c69f286d52f99036d4627e99bf91b
gopipatel123 <155124002+gopipatel123@users.noreply.github.com>
2024-01-11 08:53:01 +0530

    Add files via upload

---
+++ string.php
@@ -0,0 +1,120 @@
+<?php
+
+// $a=strlen(" Hello, World! ");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=strpos(" Hello, World! ","World");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=str_replace("world","universe","hello world!");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=substr("hello,world!",0.5);
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=strtolower("hello ,world!");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=strtoupper("hello ,world!");
+
+//echo $a;
+
+?>
+<?php
+
+//$a=ucfirst("hello, world!");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=strrev("hello ,world!");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=trim("hello ,world!");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=date("Y-m-d H:i:s");
+
+//echo $a;
+
+
+?>
+
+<?php
+
+//$a=time();
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=strtotime("2024-08-05");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=gmdate("Y-m-d H:i:s");
+
+//echo $a;
+
+?>
+
+<?php
+
+//$a=mktime(12,0,0,8,1,2023);
+
+//echo $a;
+
+?>
+
+
+<?php
+
+
+$a=date_default_timezone_set("america/new_york");
+
+echo $a;
+?>
