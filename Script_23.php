ff9c18b033de9d043fb41ea0fc1697b015e9dbef
gopipatel123 <155124002+gopipatel123@users.noreply.github.com>
2024-02-22 08:10:48 +0530

    Delete Script_23.php

--- Script_23.php
+++
@@ -1,9 +0,0 @@
-<?php
-$num1 = 10; 
-$num2 = 25;
-$num3 = 15; 
-
-$max = max($num1, $num2, $num3); 
-
-echo "The largest number among " . $num1 . ", " . $num2 . ", and " . $num3 . " is " . $max;
-?>
